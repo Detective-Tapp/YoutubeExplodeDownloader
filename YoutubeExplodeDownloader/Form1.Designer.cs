95bd1dd70edd25a0f8dbe2d2b4b8d98c23193def
Detective-Tapp <burwessel@gmail.com>
2024-06-03 21:40:00 +0200

    Add project files.

---
+++ YoutubeExplodeDownloader/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace YoutubeExplodeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SearchBtn = new Button();
+            UrlTxt = new TextBox();
+            PathTxt = new TextBox();
+            pictureBox1 = new PictureBox();
+            DownloadProgress = new ProgressBar();
+            PlaylistProgress = new ProgressBar();
+            DownloadLbl = new Label();
+            PlaylistLbl = new Label();
+            UrlLbl = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(354, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Download Folder";
+            // 
+            // SearchBtn
+            // 
+            SearchBtn.Location = new Point(713, 12);
+            SearchBtn.Name = "SearchBtn";
+            SearchBtn.Size = new Size(75, 76);
+            SearchBtn.TabIndex = 1;
+            SearchBtn.Text = "Search";
+            SearchBtn.UseVisualStyleBackColor = true;
+            SearchBtn.Click += SearchBtn_Click;
+            // 
+            // UrlTxt
+            // 
+            UrlTxt.Location = new Point(12, 12);
+            UrlTxt.Name = "UrlTxt";
+            UrlTxt.Size = new Size(695, 23);
+            UrlTxt.TabIndex = 2;
+            // 
+            // PathTxt
+            // 
+            PathTxt.Location = new Point(354, 65);
+            PathTxt.Name = "PathTxt";
+            PathTxt.Size = new Size(353, 23);
+            PathTxt.TabIndex = 3;
+            PathTxt.Click += PathTxt_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 38);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(336, 279);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // DownloadProgress
+            // 
+            DownloadProgress.Location = new Point(688, 94);
+            DownloadProgress.Name = "DownloadProgress";
+            DownloadProgress.Size = new Size(100, 23);
+            DownloadProgress.Step = 1;
+            DownloadProgress.TabIndex = 5;
+            // 
+            // PlaylistProgress
+            // 
+            PlaylistProgress.Location = new Point(354, 123);
+            PlaylistProgress.Name = "PlaylistProgress";
+            PlaylistProgress.Size = new Size(434, 23);
+            PlaylistProgress.Step = 1;
+            PlaylistProgress.TabIndex = 6;
+            // 
+            // DownloadLbl
+            // 
+            DownloadLbl.AutoSize = true;
+            DownloadLbl.Location = new Point(583, 94);
+            DownloadLbl.Name = "DownloadLbl";
+            DownloadLbl.Size = new Size(99, 15);
+            DownloadLbl.TabIndex = 7;
+            DownloadLbl.Text = "Downloading -->";
+            DownloadLbl.Visible = false;
+            // 
+            // PlaylistLbl
+            // 
+            PlaylistLbl.AutoSize = true;
+            PlaylistLbl.Location = new Point(606, 109);
+            PlaylistLbl.Name = "PlaylistLbl";
+            PlaylistLbl.Size = new Size(24, 15);
+            PlaylistLbl.TabIndex = 8;
+            PlaylistLbl.Text = "0/0";
+            // 
+            // UrlLbl
+            // 
+            UrlLbl.AutoSize = true;
+            UrlLbl.Font = new Font("Microsoft Sans Serif", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            UrlLbl.Location = new Point(397, 200);
+            UrlLbl.Name = "UrlLbl";
+            UrlLbl.Size = new Size(271, 46);
+            UrlLbl.TabIndex = 9;
+            UrlLbl.Text = "Checking Link";
+            UrlLbl.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 333);
+            Controls.Add(UrlLbl);
+            Controls.Add(DownloadLbl);
+            Controls.Add(PlaylistProgress);
+            Controls.Add(DownloadProgress);
+            Controls.Add(pictureBox1);
+            Controls.Add(PathTxt);
+            Controls.Add(UrlTxt);
+            Controls.Add(SearchBtn);
+            Controls.Add(label1);
+            Controls.Add(PlaylistLbl);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button SearchBtn;
+        private TextBox UrlTxt;
+        private TextBox PathTxt;
+        private PictureBox pictureBox1;
+        private ProgressBar DownloadProgress;
+        private ProgressBar PlaylistProgress;
+        private Label DownloadLbl;
+        private Label PlaylistLbl;
+        private Label UrlLbl;
+    }
+}
---
+++ YoutubeExplodeDownloader/Form1.cs
@@ -0,0 +1,143 @@
+﻿using TagLib;
+using System.Text.RegularExpressions;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Converter;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace YoutubeExplodeDownloader
+{
+    public partial class Form1 : Form
+    {
+
+        YoutubeClient youtube;
+        //Cookie cookies = new Cookie();
+
+        string[] illegalChars = new string[] {"#","@","%","{","}","\\","<",">","*",
+                                              "/", "`","|", ":", ";"};
+
+        public Form1()
+        {
+            InitializeComponent();
+            //youtube = new YoutubeClient(cookies);
+            youtube = new YoutubeClient();
+        }
+
+        private async void GetVideo(string url)
+        {
+            DownloadProgress.Value = 0;
+            DownloadProgress.Refresh();
+
+            StreamManifest streamManifest;
+            try
+            {
+                streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    GetPlaylist(url);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                return;
+            }
+            UrlLbl.Visible = false;
+            UrlLbl.Refresh();
+
+            DownloadLbl.Visible = true;
+            DownloadLbl.Refresh();
+
+            var video = await youtube.Videos.GetAsync(url);
+            var title = Regex.Replace(video.Title, @"[^a-zA-Z0-9\-]", "");
+            pictureBox1.ImageLocation = video.Thumbnails.Where(t => t.Url.EndsWith(".jpg")).GetWithHighestResolution().Url;
+            pictureBox1.Refresh();
+
+            var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            var progress = new Progress<double>(p =>
+            {
+                DownloadProgress.Value = Convert.ToInt32(p * 100);
+                var percentage = (p * 100);
+                var percentage2 = Math.Truncate(percentage) / 1;
+            }); 
+            if (!Directory.GetFiles(PathTxt.Text).Contains($"{PathTxt.Text}\\{title}.mp3"))
+                await youtube.Videos.DownloadAsync(url, $"{PathTxt.Text.ToString()}\\{title}.mp3", progress);
+            pictureBox1.Refresh();
+            AddCover(title, video);
+
+            DownloadLbl.Visible = false;
+            DownloadLbl.Refresh();
+        }
+
+        private void AddCover(string title, Video video)
+        {
+            var tfile = TagLib.File.Create($"{PathTxt.Text}\\{title}.mp3");
+            tfile.Tag.Pictures = new IPicture[] { new Picture(new ByteVector((byte[])new ImageConverter().ConvertTo(pictureBox1.Image, typeof(byte[])))) };
+            tfile.Tag.Title = video.Title;
+            tfile.Tag.DateTagged = DateTime.Now;
+            tfile.Save();
+            tfile.Dispose();
+        }
+       
+        private async void GetPlaylist(string url)
+        {
+            try
+            {
+                var playlist = await youtube.Playlists.GetAsync(url);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            PlaylistProgress.Value = 0;
+            PlaylistProgress.Refresh();
+
+
+            // Get all playlist videos
+            var videos = await youtube.Playlists.GetVideosAsync(url);
+            PlaylistProgress.Maximum = videos.Count;
+            PlaylistLbl.Text = "0/" + videos.Count.ToString();
+            PlaylistLbl.Refresh();
+            foreach (var video in videos)
+            {
+                PlaylistLbl.Text = $"{PlaylistProgress.Value}/{PlaylistProgress.Maximum}";
+                PlaylistLbl.Refresh();
+                PlaylistProgress.PerformStep();
+                GetVideo(video.Url);
+            }
+            PlaylistLbl.Text = $"{PlaylistProgress.Value}/{PlaylistProgress.Maximum}";
+            PlaylistLbl.Refresh();
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            UrlLbl.Visible = true;
+            UrlLbl.Refresh();
+            if (PathTxt.Text != string.Empty && UrlTxt.Text != string.Empty)
+                GetVideo(UrlTxt.Text);
+            else
+                MessageBox.Show("Please select a directory and provide a link");
+        }
+
+        private void PathTxt_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                PathTxt.Text = diag.SelectedPath;
+                // Focus on the form so you can interract with the other elements without issue.
+                Focus();
+            }
+        }
+    }
+}
